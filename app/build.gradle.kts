c962575516dd143e951e185652f22e634b54b758
haraldmandl <harald.mandl@edu.fh-joanneum.at>
2024-12-03 18:57:33 +0100

    Implement Retrofit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,7 +41,8 @@ android {
 }
 
 dependencies {
-
+    implementation(libs.retrofit.core)
+    implementation(libs.retrofit.gson)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
